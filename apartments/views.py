d90e1706472ec08822196bbbea4da550eab4712b
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-08-21 17:35:18 +0200

    feat: add apartmentfilters and empty state

--- apartments/views.py
+++ apartments/views.py
@@ -4,12 +4,53 @@ from .models import Apartment
 
 def apartment_list(request):
     apartments = Apartment.objects.all()
+    selected_city = request.GET.get("city", "")
+    min_price = request.GET.get("min_price", "")
+    max_price = request.GET.get("max_price", "")
+    min_area = request.GET.get("min_area", "")
+    max_area = request.GET.get("max_area", "")
+    rooms_number = request.GET.get("rooms_number", "")
+    floor_number = request.GET.get("floor_number", "")
+
+    if selected_city:
+        apartments = apartments.filter(city__iexact=selected_city)
+
+    if min_price:
+        apartments = apartments.filter(price__gte=min_price)
+
+    if max_price:
+        apartments = apartments.filter(price__lte=max_price)
+
+    if min_area:
+        apartments = apartments.filter(area__gte=min_area)
+
+    if max_area:
+        apartments = apartments.filter(area__lte=max_area)
+
+    if rooms_number:
+        if rooms_number == "5_plus":
+            apartments = apartments.filter(rooms__gte=5)
+        else:
+            apartments = apartments.filter(rooms=rooms_number)
+
+    if floor_number:
+        if floor_number == "5_plus":
+            apartments = apartments.filter(floor__gte=5)
+        else:
+            apartments = apartments.filter(floor=floor_number)
 
     return render(
         request,
         'apartments/apartment_list.html',
         {
             "apartments": apartments,
+            "selected_city": selected_city,
+            "min_price": min_price,
+            "max_price": max_price,
+            "min_area": min_area,
+            "max_area": max_area,
+            "rooms_number": rooms_number,
+            "floor_number": floor_number,
         }
     )
 
