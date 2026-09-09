881beeae862b8e9b160fccadf218c3060cf7bc35
Piotr Golebiewski <pgolebiewski07@gmail.com>
2026-09-09 10:43:11 +0200

    feat: add apartment analysis fields

--- apartments/models.py
+++ apartments/models.py
@@ -19,13 +19,31 @@ class Apartment(models.Model):
     price = models.DecimalField(max_digits=12, decimal_places=2)
     area = models.DecimalField(max_digits=6, decimal_places=2)
     rooms = models.PositiveSmallIntegerField()
-    floor = models.PositiveSmallIntegerField()
+    floor = models.PositiveSmallIntegerField(null=True, blank=True)
     floor_count = models.PositiveSmallIntegerField(null=True, blank=True)
     year_built = models.PositiveSmallIntegerField(null=True, blank=True)
     market_type = models.CharField(max_length=20, choices=MARKET_CHOICES, blank=True, default="")
     has_balcony = models.BooleanField(null=True, blank=True)
     has_elevator = models.BooleanField(null=True, blank=True)
     has_parking = models.BooleanField(null=True, blank=True)
+    city_median_price_per_sqm = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+
+    price_difference_pct = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        null=True,
+        blank=True,
+    )
+
+    price_rating = models.CharField(
+        max_length=30,
+        blank=True,
+    )
 
     @property
     def price_per_sqm(self):
